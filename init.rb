aa364bb47e023f0ab2df54f52b00581769abb266
Matthew Gordon <gordon@jeremiad.local>
2009-09-13 17:39:42 -0400

    made has_many_unified_attachments work; made the overrides for has_one_unified_attachment more complete so it covers all the cases

--- init.rb
+++ init.rb
@@ -1,53 +1,85 @@
-module ActiveRecord
-  module Associations
-    module ClassMethods
-      alias :ar_has_one :has_one
-      def has_one(association_id, options = {})
-        ares_return_value = ar_has_one(association_id, options)
-        self.unify_attachments(association_id) if options[:class_name] == "UnifiedUpload"
-        ares_return_value
-      end
+module UnifiedAttachmentAssociationMethods
+  def generate_flavor(association_id)
+    "#{self.class.name}_#{association_id.to_s}"
+  end
+  
+  def has_one_unified_attachment(association_id, options = {})
+    default_options = { 
+      :as          => :uploadable,
+      :class_name  => "UnifiedUpload",
+      :foreign_key => "uploadable_id", 
+      :conditions  => {:flavor => self.generate_flavor(association_id)}          
+    }
+
+    # gotta dupe before we merge conditions
+    options = options.dup.symbolize_keys
+    
+    if options[:conditions]
+      options[:conditions] = options[:conditions].dup.symbolize_keys
+      options[:conditions].reverse_merge!(default_options[:conditions])
+    end        
+
+    options.reverse_merge!(default_options)        
+    tmp = has_one(association_id, options)
+    self.unify_has_one_attachments(association_id)
+    tmp
+  end
+
+  def has_many_unified_attachments(association_id, options = {})
+    flavor = self.generate_flavor(association_id)
+    default_options = { 
+      :as          => :uploadable,
+      :class_name  => "UnifiedUpload",
+      :foreign_key => "uploadable_id", 
+      :conditions  => {:flavor => flavor},
+      :before_add  => Proc.new {|parent, item| item.flavor = flavor }
+    }
+
+    # we want these to stack
+    if options[:before_add] && options[:before_add].is_a?(Array)
+      options[:before_add] << default_options[:before_add] 
+    elsif options[:before_add]
+      options[:before_add] = [options[:before_add]]
+      options[:before_add] << default_options[:before_add] 
+    end 
+    
+    # gotta dupe before we merge conditions
+    options = options.dup.symbolize_keys
+    
+    if options[:conditions]
+      options[:conditions] = options[:conditions].dup.symbolize_keys
+      options[:conditions].reverse_merge!(default_options[:conditions])
+    end        
+
+    options.reverse_merge!(default_options)
+    
+    has_many(association_id, options)
+  end
+  
+  def unify_has_one_attachments(association_id)
+    module_eval do 
+      alias :"ar_#{association_id}=" :"#{association_id}="
+      alias :"ar_build_#{association_id}" :"build_#{association_id}"
+    end
       
-      def has_unified_attachment(association_id, options = {})
-        default_options = { 
-          :as => :uploadable,
-          :class_name => "UnifiedUpload",
-          :foreign_key => "uploadable_id", 
-          :conditions => {:flavor => "#{self.name}_#{association_id.to_s}"}          
-        }
-
-        # gotta dupe before we merge conditions
-        options = options.dup.symbolize_keys
-        
-        if options[:conditions]
-          options[:conditions] = options[:conditions].dup.symbolize_keys
-          options[:conditions].reverse_merge!(default_options[:conditions])
-        end        
-
-        options.reverse_merge!(default_options)        
-        has_one(association_id, options)
-      end
+    define_method("#{association_id}=") do |*params|
+      params.first.flavor = self.generate_flavor(association_id)
+      self.send("ar_#{association_id}=", *params)
+    end
       
-      def has_many_unified_attachments(association_id, options = {})
-        default_options = { 
-          :as => :uploadable,
-          :class_name => "UnifiedUpload",
-          :foreign_key => "uploadable_id", 
-          :conditions => {:flavor => "#{self.name}_#{association_id.to_s}"}          
-        }
-
-        # gotta dupe before we merge conditions
-        options = options.dup.symbolize_keys
-        
-        if options[:conditions]
-          options[:conditions] = options[:conditions].dup.symbolize_keys
-          options[:conditions].reverse_merge!(default_options[:conditions])
-        end        
-
-        options.reverse_merge!(default_options)        
-        has_many(association_id, options)
-      end
-
+    define_method("build_#{association_id}") do |*params|
+      params << {} unless params.first
+      params.first[:flavor] = self.generate_flavor(association_id)
+      self.send("ar_build_#{association_id}", *params)
+    end
+    
+    define_method("create_#{association_id}") do |*params|
+      params << {} unless params.first
+      params.first[:flavor] = self.generate_flavor(association_id)
+      self.send("ar_build_#{association_id}", *params)
     end
   end
+
 end
+
+ActiveRecord::Base.extend(UnifiedAttachmentAssociationMethods)
