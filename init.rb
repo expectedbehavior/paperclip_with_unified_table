94d78cca816cfc0c5b5697576cea3abde1757b09
Matthew Gordon <gordon@jeremiad.local>
2009-09-13 16:25:53 -0400

    trying out letting you automatically have many attachments at once

--- init.rb
+++ init.rb
@@ -27,6 +27,27 @@ module ActiveRecord
         options.reverse_merge!(default_options)        
         has_one(association_id, options)
       end
+      
+      def has_many_unified_attachments(association_id, options = {})
+        default_options = { 
+          :as => :uploadable,
+          :class_name => "UnifiedUpload",
+          :foreign_key => "uploadable_id", 
+          :conditions => {:flavor => "#{self.name}_#{association_id.to_s}"}          
+        }
+
+        # gotta dupe before we merge conditions
+        options = options.dup.symbolize_keys
+        
+        if options[:conditions]
+          options[:conditions] = options[:conditions].dup.symbolize_keys
+          options[:conditions].reverse_merge!(default_options[:conditions])
+        end        
+
+        options.reverse_merge!(default_options)        
+        has_many(association_id, options)
+      end
+
     end
   end
 end
